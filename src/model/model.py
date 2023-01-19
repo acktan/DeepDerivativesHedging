798c340eedf13bfff55dbb81637c9efd2fbfb906
Lea Chader <lea.chader@hec.edu>
2023-01-19 09:54:56 +0000

    Changes to Heston model
    Signed-off-by: Lea Chader <lea.chader@hec.edu>

--- src/model/model.py
+++ src/model/model.py
@@ -50,17 +50,19 @@ class DeepHedging_Hest(nn.Module):
     """
     def __init__(self, conf):
         super(DeepHedging_Hest, self).__init__()
-        input_size = conf["Hest_model"]["input_size"]
-        HL_size = conf["Hest_model"]["HL_size"]
-        output_size = conf["Hest_model"]["output_size"]
-        self.rnn = torch.nn.RNN(input_size=input_size,
-                                hidden_size=HL_size,
-                                num_layers=1)
-        self.linear = torch.nn.Linear(HL_size, output_size)
-        #self.softplus = torch.nn.Softplus()
+        self.input_size = conf["Hest_model"]["input_size"]
+        self.HL_size = conf["Hest_model"]["HL_size"]
+        self.output_size = conf["Hest_model"]["output_size"]
+        self.num_layers = conf["Hest_model"]["num_layers"]
+        self.rnn = torch.nn.GRU(input_size=self.input_size,
+                                hidden_size=self.HL_size,
+                                num_layers=self.num_layers,
+                                batch_first=True)
+        self.linear = torch.nn.Linear(self.HL_size, self.output_size)
     def forward(self, S):
         """Create the forward function for the model."""
-        out, _ = self.rnn(S)
+        h0 = torch.empty(self.num_layers, S.size(0), self.HL_size)
+        h0 = torch.nn.init.xavier_uniform_(h0).requires_grad_()
+        out, _ = self.rnn(S, h0)
         out = self.linear(out)
-        #out = self.softplus(out)
-        return out.squeeze()  
+        return out.squeeze() 
